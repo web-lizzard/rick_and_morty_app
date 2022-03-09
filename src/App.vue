9b72adf4adfa4f84eed982cdc827f6e6d31ae6a4
Adrian <adrian.czarnous@antologic.com>
2022-03-09 20:50:07 +0100

    add scss basic config, remove storybook boilerplate

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,3 @@
 <template>
   <router-view />
 </template>
-
-<style>
-
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,4 +3,14 @@
 
 <template>
   <div>placeholder</div>
+  <h1>test clamp fs</h1>
 </template>
+
+
+<style lang="scss" scoped>
+
+h1 {
+  font-size: var(--fs-500);
+}
+
+</style>
